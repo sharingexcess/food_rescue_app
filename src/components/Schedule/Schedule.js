6b97f06e48a6713ef2f0b50ce6065a28f21366b3
Ryan McHenry <rmchenry@izotope.com>
2020-12-21 15:35:15 -0500

    improvements to ui

--- src/components/Schedule/Schedule.js
+++ src/components/Schedule/Schedule.js
@@ -46,7 +46,9 @@ function Schedule() {
           <tbody>
             {rescues.map(r => (
               <tr key={r.id}>
-                <td>{r.id}</td>
+                <td>
+                  <Link to={`rescues/${r.id}`}>{r.id}</Link>
+                </td>
                 <td>{r.pickup_timestamp.toString()}</td>
                 <td>{r.delivery_timestamp.toString()}</td>
               </tr>
