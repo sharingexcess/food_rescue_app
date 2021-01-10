6784a0abff8f2ea66062ff7f4627609bb8d23766
Ryan McHenry <rmchenry@izotope.com>
2021-01-10 13:23:32 -0500

    adding full statuses for driver ux interactions

--- src/helpers/constants.js
+++ src/helpers/constants.js
@@ -9,27 +9,47 @@ export const FIREBASE_CONFIG = {
   appId: process.env.REACT_APP_FIREBASE_APP_ID,
   measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID,
 }
-
 // 600 pixels is our baseline threshold for handling a mobile screen vs. desktop
 export const MOBILE_THRESHOLD = 600
 
-// these values map a rescue's 'status' property to a literal meaning.
-// 0, 3, 6, and 9 are currently used, with the other number remaining open for future use cases
-export const RESCUE_STATUSES = {
-  0: 'scheduled',
-  1: 'UNDEFINED_STATUS',
+export const PICKUP_STATUSES = {
+  0: 'cancelled',
+  1: 'scheduled',
   2: 'UNDEFINED_STATUS',
-  3: 'assigned',
+  3: 'in_progress',
   4: 'UNDEFINED_STATUS',
   5: 'UNDEFINED_STATUS',
-  6: 'reported',
+  6: 'arrived',
   7: 'UNDEFINED_STATUS',
   8: 'UNDEFINED_STATUS',
   9: 'completed',
 }
 
-const test_url =
-  'http://localhost:5001/sharing-excess-7e887/us-central1/backend/'
+export const DELIVERY_STATUSES = {
+  0: 'cancelled',
+  1: 'scheduled',
+  2: 'UNDEFINED_STATUS',
+  3: 'in_progress',
+  4: 'UNDEFINED_STATUS',
+  5: 'UNDEFINED_STATUS',
+  6: 'arrived',
+  7: 'UNDEFINED_STATUS',
+  8: 'UNDEFINED_STATUS',
+  9: 'completed',
+}
+
+export const ROUTE_STATUSES = {
+  0: 'cancelled',
+  1: 'scheduled',
+  2: 'UNDEFINED_STATUS',
+  3: 'in_progress',
+  4: 'UNDEFINED_STATUS',
+  5: 'UNDEFINED_STATUS',
+  6: 'UNDEFINED_STATUS',
+  7: 'UNDEFINED_STATUS',
+  8: 'UNDEFINED_STATUS',
+  9: 'completed',
+}
 
 export const CLOUD_FUNCTION_URLS = {
   isUserAdmin: process.env.REACT_APP_CLOUD_FUNCTION_BASE_URL + 'isUserAdmin',
@@ -38,6 +58,8 @@ export const CLOUD_FUNCTION_URLS = {
     process.env.REACT_APP_CLOUD_FUNCTION_BASE_URL + 'isUserBasicAccess',
   setUserBasicAccess:
     process.env.REACT_APP_CLOUD_FUNCTION_BASE_URL + 'setUserBasicAccess',
-  addCalendarEvent: test_url + 'addCalendarEvent',
-  deleteCalendarEvent: test_url + 'deleteCalendarEvent',
+  addCalendarEvent:
+    process.env.REACT_APP_CLOUD_FUNCTION_BASE_URL + 'addCalendarEvent',
+  deleteCalendarEvent:
+    process.env.REACT_APP_CLOUD_FUNCTION_BASE_URL + 'deleteCalendarEvent',
 }
