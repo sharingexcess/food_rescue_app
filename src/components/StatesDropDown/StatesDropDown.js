e9194379c0e2fc7080a6b6d7473ad6253120c52a
Chi Nguyen <phuongchi2782001@gmail.com>
2021-01-11 10:14:39 -0500

    feat: states dropdown box

--- src/components/StatesDropDown/StatesDropDown.js
+++ src/components/StatesDropDown/StatesDropDown.js
@@ -1,22 +1,37 @@
 import React, { useState } from 'react'
+import { usStates } from './usstates'
+import './StatesDropDown.scss'
+export default function StatesDropDown({
+  element_id,
+  value,
+  onChange,
+  animation = true,
+}) {
+  const [states] = useState(usStates)
 
-export default function StatesDropDown() {
-  const [states] = useState([
-    {
-      name: 'Test',
-      abbreviation: 'TS'
-    }
-  ]);
   return (
-    <select>
-   {states.map(state => (
-        <option
-        key={state.abbreviation}
-        value={state.abbreviation}>
-        {state.name} ({state.abbreviation})
-                </option>
-            ))}
-        </select>
-    )
-    
+    <div className={`Input ${animation ? 'animation' : ''} select`}>
+      <label className="focused">States*</label>
+      <select id={element_id} onChange={onChange}>
+        {states.map(state => {
+          if (value === state.abbreviation) {
+            return (
+              <option
+                key={state.abbreviation}
+                value={state.abbreviation}
+                selected
+              >
+                {state.name} ({state.abbreviation})
+              </option>
+            )
+          }
+          return (
+            <option key={state.abbreviation} value={state.abbreviation}>
+              {state.name} ({state.abbreviation})
+            </option>
+          )
+        })}
+      </select>
+    </div>
+  )
 }
