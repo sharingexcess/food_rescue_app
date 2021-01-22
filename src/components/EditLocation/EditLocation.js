501f488be83b417430a89ef14e1976339b547203
Chi Nguyen <phuongchi2782001@gmail.com>
2021-01-22 12:38:07 -0500

    fix: delete unnecessary/wrong methods

--- src/components/EditLocation/EditLocation.js
+++ src/components/EditLocation/EditLocation.js
@@ -8,12 +8,10 @@ import { GoBack } from '../../helpers/components'
 import useOrganizationData from '../../hooks/useOrganizationData'
 import useLocationData from '../../hooks/useLocationData'
 import Loading from '../Loading/Loading'
-import validator from 'validator'
 import GoogleAutoComplete from '../GoogleAutoComplete/GoogleAutoComplete'
 import GoogleMap from '../GoogleMap/GoogleMap'
 import './EditLocation.scss'
 import validator from 'validator'
-import StatesDropDown from '../StatesDropDown/StatesDropDown'
 
 export default function EditLocation() {
   const { id, loc_id } = useParams()
@@ -36,10 +34,7 @@ export default function EditLocation() {
   )
   const [errors, setErrors] = useState([])
   const [showErrors, setShowErrors] = useState(false)
-<<<<<<< HEAD
-=======
   const [isInitialLoad, setIsInitialLoad] = useState(true)
->>>>>>> 1f5543bc66e4dea260c61c3208b3f0b7db0994b4
 
   useEffect(() => {
     if (isInitialLoad && location && location.name) {
@@ -55,40 +50,6 @@ export default function EditLocation() {
   }
 
   function validateFormData() {
-<<<<<<< HEAD
-    if (formData.name === '') {
-      errors.push('Missing Location Name')
-    }
-    if (formData.address1 === '') {
-      errors.push('Missing Address')
-    }
-    if (!validator.isAlpha(formData.city)) {
-      errors.push('Invalid City')
-    }
-    if (!validator.isPostalCode(formData.zip_code, 'US')) {
-      errors.push('Invalid Zip Code')
-    }
-    if (formData.upon_arrival_instructions === '') {
-      errors.push('Missing Upon Arrival Instructions')
-    }
-    // OPTIONAL FIELDS: check if they're empty, if not, they'll be validated
-    if (
-      !formData.contact_name === '' &&
-      !validator.isAlpha(formData.contact_name)
-    ) {
-      errors.push('Invalid Contact name')
-    }
-    if (
-      !formData.contact_phone.length === '' &&
-      !validator.isMobilePhone(formData.contact_phone)
-    ) {
-      errors.push('Invalid Contact phone')
-    }
-    if (errors.length === 0) {
-      return true
-    }
-    return false
-=======
     const updatedErrors = [...errors]
     if (formData.name === '') {
       updatedErrors.push('Missing Location Name')
@@ -96,19 +57,7 @@ export default function EditLocation() {
     if (formData.address1 === '') {
       updatedErrors.push('Missing Address')
     }
-    if (!validator.isAlpha(formData.city)) {
-      updatedErrors.push('Invalid City')
-    }
-    if (!validator.isPostalCode(formData.zip_code, 'US')) {
-      updatedErrors.push('Invalid Zip Code')
-    }
     // OPTIONAL FIELDS: check if they're empty, if not, they'll be validated
-    if (
-      formData.contact_name !== '' &&
-      !validator.isAlpha(formData.contact_name)
-    ) {
-      updatedErrors.push('Invalid Contact name')
-    }
     if (
       formData.contact_phone !== '' &&
       !validator.isMobilePhone(formData.contact_phone)
@@ -117,7 +66,6 @@ export default function EditLocation() {
     }
     setErrors(updatedErrors)
     return updatedErrors.length === 0
->>>>>>> 1f5543bc66e4dea260c61c3208b3f0b7db0994b4
   }
 
   async function handleSubmit() {
@@ -170,13 +118,10 @@ export default function EditLocation() {
         </p>
       ))
     } else return null
-<<<<<<< HEAD
-=======
   }
 
   function handleReceiveAddress(address) {
     setFormData(prevData => ({ ...prevData, ...address }))
->>>>>>> 1f5543bc66e4dea260c61c3208b3f0b7db0994b4
   }
 
   return !location ? (
@@ -193,92 +138,6 @@ export default function EditLocation() {
         value={formData.name}
         onChange={handleChange}
       />
-<<<<<<< HEAD
-      <Input
-        type="text"
-        label="Contact Name"
-        element_id="contact_name"
-        value={formData.contact_name}
-        onChange={handleChange}
-      />
-      <Input
-        type="tel"
-        label="Contact Phone"
-        element_id="contact_phone"
-        value={formData.contact_phone}
-        onChange={handleChange}
-      />
-      <Input
-        type="text"
-        label="Address Line 1 *"
-        element_id="address1"
-        value={formData.address1}
-        onChange={handleChange}
-      />
-      <Input
-        type="text"
-        label="Address Line 2"
-        element_id="address2"
-        value={formData.address2}
-        onChange={handleChange}
-      />
-      <Input
-        type="text"
-        label="City *"
-        element_id="city"
-        value={formData.city}
-        onChange={handleChange}
-      />
-      {/* <Input
-        type="text"
-        label="State *"
-        element_id="state"
-        value={formData.state}
-        onChange={handleChange}
-      /> */}
-      <StatesDropDown
-        onChange={handleChange}
-        element_id="state"
-        value={formData.state}
-      />
-      <Input
-        type="text"
-        label="Zip Code *"
-        element_id="zip_code"
-        value={formData.zip_code}
-        onChange={handleChange}
-      />
-      <Input
-        type="text"
-        label="Upon Arrival Instructions *"
-        element_id="upon_arrival_instructions"
-        value={formData.upon_arrival_instructions}
-        onChange={handleChange}
-      />
-      <div className="is_primary">
-        <input
-          type="checkbox"
-          id="is_primary"
-          name="is_primary"
-          checked={isPrimary}
-          onChange={() => setIsPrimary(!isPrimary)}
-        />
-        <p>
-          Make this the Organization's
-          <br />
-          Primary Address
-        </p>
-      </div>
-      <FormError />
-      <button
-        onClick={() => {
-          handleSubmit()
-          setShowErrors(true)
-        }}
-      >
-        {loc_id ? 'update location' : 'add location'}
-      </button>
-=======
       {formData.address1 ? (
         <>
           {formData.lat && formData.lng ? (
@@ -350,7 +209,6 @@ export default function EditLocation() {
       ) : (
         <GoogleAutoComplete handleSelect={handleReceiveAddress} />
       )}
->>>>>>> 1f5543bc66e4dea260c61c3208b3f0b7db0994b4
     </main>
   )
 }
