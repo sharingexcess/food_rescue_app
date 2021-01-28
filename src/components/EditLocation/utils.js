8f7faef5de0eb884039fd7272550deb4cc9f1432
Tommy Bui Nguyen <tbnnumbers@gmail.com>
2021-01-28 10:46:52 -0600

    feat: add secondary contact location form field

--- src/components/EditLocation/utils.js
+++ src/components/EditLocation/utils.js
@@ -3,6 +3,9 @@ export function initializeFormData(location, callback) {
     name: location.name,
     contact_name: location.contact_name ? location.contact_name : '',
     contact_phone: location.contact_phone ? location.contact_phone : '',
+    secondary_contact_phone: location.secondary_contact_phone
+      ? location.secondary_contact_phone
+      : '',
     address1: location.address1,
     address2: location.address2,
     city: location.city,
