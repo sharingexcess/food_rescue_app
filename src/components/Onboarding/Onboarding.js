c55f1909053ac00bdebaa2b10968ce60d7cbfee1
Nom Phan <np656@drexel.edu>
2021-02-08 14:19:40 -0500

    Checkbox added

--- src/components/Onboarding/Onboarding.js
+++ src/components/Onboarding/Onboarding.js
@@ -1,12 +1,27 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import FoodSafety from '../FoodSafety/FoodSafety'
+import Privacy from '../Privacy/Privacy'
+import Terms from '../Terms/Terms'
 import Logo from '../../assets/logo.svg'
 import './Onboarding.scss'
 import '../Auth/Auth.scss'
 import '../FoodSafety/FoodSafety.scss'
+import '../Privacy/Privacy.scss'
+import '../Terms/Terms.scss'
 
 export default function Onboarding(props) {
   const [page, setPage] = useState(1)
+  const [isCheck, setCheck] = useState(false)
+  function FormError() {
+    if (isCheck === false) {
+      return <p id="FormError">Please check the checkbox to proceed</p>
+    } else return null
+  }
+  // scroll to top on page change
+  useEffect(() => {
+    window.scrollTo(0, 0)
+    setCheck(false)
+  }, [page])
   return page === 1 ? (
     <main id="Onboarding">
       <img src={Logo} alt="Sharing Excess Logo" />
@@ -21,16 +36,76 @@ export default function Onboarding(props) {
   ) : page === 2 ? (
     <main id="FoodSafety">
       <FoodSafety />
+      <div id="Checkbox">
+        <input
+          type="checkbox"
+          id="foodsafety"
+          checked={isCheck}
+          onChange={() => setCheck(!isCheck)}
+        />
+        <p className="inner">Agree to Food Safety Training</p>
+      </div>
+      <FormError />
       <div id="Navigation">
         <div className="inner">
           <button onClick={() => setPage(page - 1)}>Back</button>
         </div>
         <div className="inner">
-          <button onClick={() => setPage(page + 1)}>Next</button>
+          <button disabled={!isCheck} onClick={() => setPage(page + 1)}>
+            Next
+          </button>
         </div>
       </div>
     </main>
   ) : page === 3 ? (
+    <main id="Privacy">
+      <Privacy />
+      <div id="Checkbox">
+        <input
+          type="checkbox"
+          id="privacy"
+          checked={isCheck}
+          onChange={() => setCheck(!isCheck)}
+        />
+        <p className="inner">Agree to Privacy Policy</p>
+      </div>
+      <FormError />
+      <div id="Navigation">
+        <div className="inner">
+          <button onClick={() => setPage(page - 1)}>Back</button>
+        </div>
+        <div className="inner">
+          <button disabled={!isCheck} onClick={() => setPage(page + 1)}>
+            Next
+          </button>
+        </div>
+      </div>
+    </main>
+  ) : page === 4 ? (
+    <main id="Terms">
+      <Terms />
+      <div id="Checkbox">
+        <input
+          type="checkbox"
+          id="terms"
+          checked={isCheck}
+          onChange={() => setCheck(!isCheck)}
+        />
+        <p className="inner">Agree to Terms and Conditions</p>
+      </div>
+      <FormError />
+      <div id="Navigation">
+        <div className="inner">
+          <button onClick={() => setPage(page - 1)}>Back</button>
+        </div>
+        <div className="inner">
+          <button disabled={!isCheck} onClick={() => setPage(page + 1)}>
+            Next
+          </button>
+        </div>
+      </div>
+    </main>
+  ) : page === 5 ? (
     <main id="Auth" className="request-access">
       <h1>
         <span className="green">Sharing</span> Excess
