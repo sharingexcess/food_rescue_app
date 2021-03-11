a67cc5889e26592e89b8686ef71fe4f1224dfe76
ajh387 <ajh387@drexel.edu>
2021-03-11 01:06:47 -0500

    Allow drivers to select stops in any order

--- src/components/Route/Route.js
+++ src/components/Route/Route.js
@@ -77,11 +77,15 @@ function Route() {
       route.status < 3
     )
       return false
-    return (
-      index === 0 ||
-      stops[index - 1].status === 9 ||
-      stops[index - 1].status === 0
-    )
+    let inProgress = false
+    for (const s of stops) {
+      if (s.status === 3) {
+        inProgress = true
+        break
+      }
+    }
+    if (inProgress) return stops[index].status === 3
+    return true
   }
 
   function areAllStopsCompleted() {
@@ -531,7 +535,6 @@ function Route() {
     const [cancelNotes, setCancelNotes] = useState('')
 
     function handleCancel() {
-      setCancelStop(false)
       const collection = stop.type === 'pickup' ? 'Pickups' : 'Deliveries'
       getCollection(collection)
         .doc(stop.id)
@@ -624,10 +627,18 @@ function Route() {
   function BackupDelivery() {
     const [willFind, setWillFind] = useState()
     if (areAllStopsCompleted()) {
-      const lastStop = stops[stops.length - 1]
+      let lastStop
+      for (const s of stops) {
+        if (
+          s.status === 9 &&
+          (!lastStop || s.time_started > lastStop.time_started)
+        )
+          lastStop = s
+      }
       if (
-        !lastStop.report.weight ||
-        lastStop.report.percent_of_total_dropped < 100
+        lastStop &&
+        (!lastStop.report.weight ||
+          lastStop.report.percent_of_total_dropped < 100)
       ) {
         async function addBackupDelivery(stop) {
           const stop_id = generateStopId(stop)
