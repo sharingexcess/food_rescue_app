6dbfcf5ea953a1e013f5e9bd3400989315b506a6
Tri Truong <truongductri01@gmail.com>
2021-04-13 16:43:32 -0500

    Hard coded tasks any new driver needs to do

--- src/components/InfoTodoList/InfoTodoList.js
+++ src/components/InfoTodoList/InfoTodoList.js
@@ -1,7 +1,27 @@
 import React from 'react'
+import './InfoTodoList.scss'
 
+function Task({ title, status }) {
+  // status means complete or incomplete
+  return (
+    <div className="Task">
+      {status === 'complete' ? (
+        <i className="fa fa-check StatusIndicator"></i>
+      ) : (
+        <i className="fa fa-clock-o" id="StatusIndicator"></i>
+      )}
+      <p>{title}</p>
+    </div>
+  )
+}
 function InfoTodoList() {
-  return <div>This is the Todo List</div>
+  return (
+    <div className="TodoList">
+      <Task title="Input phone Number" status="incomplete" />
+      <Task title="Insert Driver License" status="incomplete" />
+      <Task title="Insert Insurance" status="incomplete" />
+    </div>
+  )
 }
 
 export default InfoTodoList
