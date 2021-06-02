9ba06ead35c097f17d2b6b9596ff41cd746692b6
Nom Phan <np656@drexel.edu>
2021-06-01 23:08:14 -0400

    remove increment and decrement

--- src/components/PickupReport/PickupReport.js
+++ src/components/PickupReport/PickupReport.js
@@ -27,12 +27,13 @@ export default function PickupReport() {
     'prepared/Frozen': 0,
     'mixed groceries': 0,
     other: 0,
-    weight: '',
+    weight: 0,
     notes: '',
   })
   const [changed, setChanged] = useState(false)
   const [errors, setErrors] = useState([])
   const [showErrors, setShowErrors] = useState(false)
+  let updatedWeight = 0
 
   useEffect(() => {
     pickup && pickup.report
@@ -40,34 +41,37 @@ export default function PickupReport() {
       : setChanged(true)
   }, [pickup])
 
+  useEffect(() => {
+    updatedWeight = sumWeight(formData)
+  }, [changed])
+
   function canEdit() {
     return [1, 3].includes(pickup.status) || admin
   }
-
+  console.log(sumWeight(formData))
+  function sumWeight(object) {
+    let sum = 0
+    for (const field in object) {
+      if (field === 'weight' || field === 'notes') {
+        console.log('Not count >>>', field)
+      } else {
+        sum += parseFloat(object[field])
+      }
+      return sum
+    }
+  }
   function handleChange(e) {
+    // TODO: take the sum of all the field
     setErrors([])
     setShowErrors(false)
-    setFormData({ ...formData, [e.target.id]: e.target.value })
-    setChanged(true)
-  }
-
-  function increment(field) {
-    setErrors([])
-    setShowErrors(false)
-    setFormData({ ...formData, [field]: parseFloat(formData[field]) + 1 })
-    setChanged(true)
-  }
-
-  function decrement(field) {
-    setErrors([])
-    setShowErrors(false)
+    updatedWeight += parseInt(e.target.value)
     setFormData({
       ...formData,
-      [field]: Math.max(0, parseFloat(formData[field]) - 1),
+      [e.target.id]: e.target.value,
+      weight: updatedWeight,
     })
     setChanged(true)
   }
-
   function validateFormData() {
     if (
       formData.dairy +
@@ -148,6 +152,7 @@ export default function PickupReport() {
     <main id="PickupReport">
       <Header text="Rescue Report" />
       <h3>{pickup_org.name}</h3>
+      <h1 className="center">Please input weights by category</h1>
       {Object.keys(formData)
         .sort(function (a, b) {
           if (a === 'other') {
@@ -162,11 +167,6 @@ export default function PickupReport() {
           !['weight', 'notes', 'created_at', 'updated_at'].includes(field) ? (
             <section key={field}>
               <h5>{field}</h5>
-              {canEdit() ? (
-                <button className="decrement" onClick={() => decrement(field)}>
-                  -
-                </button>
-              ) : null}
               <input
                 id={field}
                 type="string"
@@ -174,23 +174,12 @@ export default function PickupReport() {
                 onChange={handleChange}
                 readOnly={!canEdit()}
               />
-              {canEdit() ? (
-                <button className="increment" onClick={() => increment(field)}>
-                  +
-                </button>
-              ) : null}
             </section>
           ) : null
         )}
       <section className="weight">
         <h4>Total Weight (lbs.)</h4>
-        <input
-          id="weight"
-          type="tel"
-          value={formData.weight}
-          onChange={handleChange}
-          readOnly={!canEdit()}
-        />
+        <h4>{formData.weight}</h4>
       </section>
       <Input
         type="textarea"
