3aa1f92585123abdbb818c3e5b6d612fe82bafce
Luke Shin <ejs364@drexel.edu>
2021-06-24 12:40:06 -0400

    Changed all instances of Organization/Org to Network; changed Manage Network icon

--- src/components/Route/Route.js
+++ src/components/Route/Route.js
@@ -492,7 +492,7 @@ export function Route() {
     let beginTime = route.time_started
       ? moment(route.time_started.toDate())
       : moment(new Date())
-    beginTime = beginTime.add(2, 'minutes')
+    beginTime = beginTime.add(15, 'minutes')
     const currentTime = moment(new Date())
     function handleOpenReport() {
       const baseURL = location.pathname.includes('routes')
