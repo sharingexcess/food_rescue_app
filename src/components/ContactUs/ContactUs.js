3b66c66b10e9f730a23b412ffb50633c689bcb58
sn867 <sn867@drexel.edu>
2021-11-02 10:58:25 -0400

    Update contact phone number

--- src/components/ContactUs/ContactUs.js
+++ src/components/ContactUs/ContactUs.js
@@ -6,7 +6,7 @@ export function ContactUs() {
       <Spacer height={32} />
       <Text type="paragraph" color="white" shadow>
         Need help? Call us at{' '}
-        <ExternalLink to="tel:1-833-7424-7397">1 (833) 7424-7397</ExternalLink>
+        <ExternalLink to="tel:1-833-742-7397">1 (833) 742-7397</ExternalLink>
         <br />
         Use the form below to submit any feedback to improve the app.
       </Text>
