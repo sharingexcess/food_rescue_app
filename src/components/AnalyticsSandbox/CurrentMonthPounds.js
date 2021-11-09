09a9f4d17d7da15929678e12495ecbe02bf629f4
Armando <ajp434@drexel.edu>
2021-11-09 15:37:51 -0500

    Added Emission Reduced, Retail Value, Fair Market Value

--- src/components/AnalyticsSandbox/CurrentMonthPounds.js
+++ src/components/AnalyticsSandbox/CurrentMonthPounds.js
@@ -1,16 +1,19 @@
 import React from 'react'
 
-import { Text } from '@sharingexcess/designsystem'
+import { div } from '@sharingexcess/designsystem'
 
 function CurrentMonthPounds() {
   const totalMonthPounds = 341358.51
+  const emissionsReduced = 10000000
+  const retailValue = 1000000
+  const fairMarketValue = 1000000
 
   return (
     <main id="Analytics">
       <section id="CurrentMonthPounds">
         <section id="Content">
-          <select>
-            <option> Current Month CurrentMonthPounds</option>
+          <select style={{ display: 'block' }}>
+            <option> Current Month Pounds</option>
           </select>
           <select>
             <option>January</option>
@@ -26,8 +29,23 @@ function CurrentMonthPounds() {
             <option>November</option>
             <option>December</option>
           </select>
-          <Text id="TotalMonthPounds">{totalMonthPounds}</Text>
+          <div id="TotalMonthPounds" formNoValidate>
+            {totalMonthPounds}
+          </div>
+          <section id="MonthReport">
+            <div id="Value">{emissionsReduced} </div>
+            <div id="Label">Emissions Reduced in Pounds</div>
+          </section>
+          <section id="MonthReport">
+            <div id="Value">${retailValue} </div>
+            <div id="Label">Retail Value</div>
+          </section>
+          <section id="MonthReport">
+            <div id="Value">${fairMarketValue} </div>
+            <div id="Label">Fair Market Value</div>
+          </section>
         </section>
+        <section id="Graph">yes</section>
       </section>
     </main>
   )
