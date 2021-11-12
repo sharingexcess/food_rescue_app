e664e237f98420523c95c85cb159912ca881d94a
Armando <ajp434@drexel.edu>
2021-11-12 08:32:59 -0500

    Graph titles and Style Changes

--- src/components/AnalyticsSandbox/CurrentMonthPounds.js
+++ src/components/AnalyticsSandbox/CurrentMonthPounds.js
@@ -18,19 +18,19 @@ export function CurrentMonthPounds() {
   const fairMarketValue = 1000000
 
   const piedata = [
-    { name: 'Group A', value: 33343.5 },
-    { name: 'Group B', value: 74590 },
-    { name: 'Group C', value: 234425 },
+    { name: 'Warehosue Outgoing', value: 33343.5 },
+    { name: 'Food Rescue', value: 74590 },
+    { name: 'Direct Link', value: 234425 },
   ]
 
-  const bardata = [{ name: 'A', x: 13, y: 23 }]
+  const bardata = [{ name: 'July 2021', x: 13, y: 23 }]
 
   const COLORS = ['#216810', '#9DA1A4', '#4EA528']
 
   return (
     <main id="Revamp">
       <section id="CurrentMonthPounds">
-        <section id="Content">
+        <section id="Content" style={{ marginLeft: '20px' }}>
           <select>
             <option>Current Month Pounds</option>
             <option>Current Year Pounds</option>
@@ -49,50 +49,59 @@ export function CurrentMonthPounds() {
             <option>November</option>
             <option>December</option>
           </select>
+          <Text
+            id="CurrentMonthPoundsLabel"
+            type="secondary-header"
+            color="green"
+            align="center"
+          >
+            {totalMonthPounds}
+          </Text>
           <section>
-            <Text type="secondary-header" color="green" align="center">
-              {totalMonthPounds}
-            </Text>
-          </section>
-          <div id="MonthReport">
             <Text type="small" color="green">
               {emissionsReduced}
             </Text>
             <Text type="small" color="black">
               Emissions Reduced in Pounds
             </Text>
-          </div>
-          <div>
+          </section>
+          <section>
             <Text type="small" color="green">
               ${retailValue}
             </Text>
             <Text type="small" color="black">
               Retail Value
             </Text>
-          </div>
-          <div>
+          </section>
+          <section>
             <Text type="small" color="green">
               ${fairMarketValue}
             </Text>
             <Text type="small" color="black">
               Fair Market Value
             </Text>
-          </div>
+          </section>
         </section>
 
         <section
           id="Content"
-          style={{ background: '#E6E8EA', padding: '20px' }}
+          style={{
+            background: '#E6E8EA',
+            paddingTop: '20px',
+            marginRight: '20px',
+          }}
         >
-          <ResponsiveContainer width="100%" height={250}>
-            <PieChart>
+          <Text type="graph-title" color="black" align="center">
+            Breakdown of pounds in July 2021
+          </Text>
+          <ResponsiveContainer width="100%" height={150}>
+            <PieChart margin={{ top: 10, right: 5, bottom: 10, left: 5 }}>
               <Pie
                 dataKey="value"
                 isAnimationActive="true"
                 data={piedata}
-                outerRadius={80}
+                outerRadius={40}
                 fill="#8884d8"
-                label
               >
                 {piedata.map((entry, index) => (
                   <Cell
@@ -101,15 +110,19 @@ export function CurrentMonthPounds() {
                   />
                 ))}
               </Pie>
-              <Legend style={{ align: 'center' }} />
+              <Legend align="center" iconType="circle" iconSize="12" />
             </PieChart>
           </ResponsiveContainer>
-          <ResponsiveContainer width="90%" height={100}>
+
+          <Text type="graph-title" color="black" align="center">
+            Forecast versus Actual Performance
+          </Text>
+          <ResponsiveContainer width="90%" height={60}>
             <BarChart layout="vertical" data={bardata}>
               <XAxis type="number" />
               <YAxis dataKey="name" type="category" scale="band" />
-              <Bar dataKey="x" stackId="a" barSize={20} fill="#9DA1A4" />
-              <Bar dataKey="y" stackId="a" barSize={20} fill="#4EA528" />
+              <Bar dataKey="x" stackId="a" barSize={15} fill="#9DA1A4" />
+              <Bar dataKey="y" stackId="a" barSize={15} fill="#4EA528" />
             </BarChart>
           </ResponsiveContainer>
         </section>
