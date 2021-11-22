cea2dab482f25d8395fc05c8dbb0c44f01c55132
Armando Parra <ajp434@drexel.edu>
2021-11-22 16:36:27 -0500

    Adding Retail and Fair Market values

--- src/components/AnalyticsSandbox/CurrentMonthPounds.js
+++ src/components/AnalyticsSandbox/CurrentMonthPounds.js
@@ -11,9 +11,42 @@ import {
   ResponsiveContainer,
 } from 'recharts'
 import { Text } from '@sharingexcess/designsystem'
-import { useCallback, useState } from 'react'
+import { useCallback, useEffect, useState } from 'react'
 import { useFirestore } from 'hooks'
-import { formatLargeNumber } from 'helpers'
+import { formatLargeNumber, calculateCategoryRatios } from 'helpers'
+
+const categories = [
+  'bakery',
+  'dairy',
+  'meat/Fish',
+  'mixed groceries',
+  'non-perishable',
+  'prepared/Frozen',
+  'produce',
+  'other',
+]
+
+const retailValues = {
+  bakery: 2.36,
+  dairy: 1.28,
+  'meat/Fish': 4.4,
+  'mixed groceries': 2.31,
+  'non-perishable': 3.19,
+  'prepared/Frozen': 4.13,
+  produce: 1.57,
+  other: 2.31,
+}
+
+const fairMarketValues = {
+  bakery: 2.14,
+  dairy: 1.42,
+  'meat/Fish': 2.77,
+  'mixed groceries': 1.62,
+  'non-perishable': 2.13,
+  'prepared/Frozen': 2.17,
+  produce: 1.13,
+  other: 1.62,
+}
 
 export function CurrentMonthPounds() {
   const months = [
@@ -32,6 +65,12 @@ export function CurrentMonthPounds() {
   ]
 
   const [currentMonth, setCurrentMonth] = useState(new Date().getMonth())
+  const [retailValue, setRetailValue] = useState(0)
+  const [fairMarketValue, setFairMarketValue] = useState(0)
+  const [totalMonthDeliveryPounds, setTotalMonthDeliveryPounds] = useState(0)
+  const [categoryRatios, setCategoryRatios] = useState(0)
+  const [monthOrYear, setMonthOrYear] = useState(true)
+  const [currentYear, setCurrentYear] = useState(new Date().getYear() + 1900)
 
   const deliveries = useFirestore(
     'deliveries',
@@ -65,63 +104,52 @@ export function CurrentMonthPounds() {
     )
   )
 
-  function findTotalWeight(a, type, i) {
-    if (i <= 0) return 0
-    return findTotalWeight(a, type, i - 1) + a[i - 1].report[type]
-  }
+  useEffect(() => {
+    function generateTotalWeight(a, type, length) {
+      if (length <= 0) return 0
+      return (
+        generateTotalWeight(a, type, length - 1) + a[length - 1].report[type]
+      )
+    }
 
-  // (total, currentDelivery) => total + currentDelivery.report.weight,
-  //   0
-  // )
-  const totalMonthDeliveryPounds = findTotalWeight(
-    deliveries,
-    'weight',
-    deliveries.length
-  )
+    if (deliveries.length)
+      setTotalMonthDeliveryPounds(
+        generateTotalWeight(deliveries, 'weight', deliveries.length)
+      )
+    else {
+      setTotalMonthDeliveryPounds(0)
+    }
+  }, [deliveries])
 
-  // pickups.reduce((total, p) => total + p.report.weight, 0)
-  const totalMonthPickupPounds = findTotalWeight(
-    pickups,
-    'weight',
-    pickups.length
-  )
+  useEffect(() => {
+    if (pickups.length) setCategoryRatios(calculateCategoryRatios(pickups))
+  }, [pickups])
 
-  const bakeryWeightPercentage =
-    findTotalWeight(pickups, 'bakery', pickups.length) / totalMonthPickupPounds
+  useEffect(() => {
+    let totalRetail = 0
+    let totalFairMarket = 0
+    for (const category of categories) {
+      const categoryWeight = totalMonthDeliveryPounds * categoryRatios[category]
+      const categoryRetailValue = categoryWeight * retailValues[category]
+      const categoryFairMarketValue =
+        categoryWeight * fairMarketValues[category]
+      totalRetail += categoryRetailValue
+      totalFairMarket += categoryFairMarketValue
+    }
+    setRetailValue(totalRetail)
+    setFairMarketValue(totalFairMarket)
+  }, [totalMonthDeliveryPounds, categoryRatios])
 
-  const dairyWeightPercentage =
-    findTotalWeight(pickups, 'dairy', pickups.length) / totalMonthPickupPounds
-
-  const meatWeightPercentage =
-    findTotalWeight(pickups, 'meat/Fish', pickups.length) /
-    totalMonthPickupPounds
-
-  const mixedGroceriesWeightPercentage =
-    findTotalWeight(pickups, 'mixed Groceries', pickups.length) /
-    totalMonthPickupPounds
-
-  const nonPerishablesWeightPercentage =
-    findTotalWeight(pickups, 'non-perishable', pickups.length) /
-    totalMonthPickupPounds
-
-  const preparedsWeightPercentage =
-    findTotalWeight(pickups, 'prepared/Frozen', pickups.length) /
-    totalMonthPickupPounds
-
-  // pickups.reduce((total, p) => total + p.report.produce, 0)
-  const produceWeightPercentage =
-    findTotalWeight(pickups, 'produce', pickups.length) / totalMonthPickupPounds
-
-  const otherPerishablesWeightPercentage =
-    findTotalWeight(pickups, 'other', pickups.length) / totalMonthPickupPounds
-
-  const emissionsReduced = 3.66 * totalMonthDeliveryPounds
-  const retailValue = 1000000
-  const fairMarketValue = 1000000
   const forecast = 460000
 
   const breadkdownOfPounds = [
-    { name: 'Warehosue Outgoing', value: 0 },
+    {
+      name: 'Warehosue Outgoing',
+      value: deliveries.reduce(
+        (total, d) => (d.direct_donation_id ? total + d.report.weight : total),
+        0
+      ),
+    },
     {
       name: 'Food Rescue',
       value: deliveries.reduce(
@@ -131,10 +159,7 @@ export function CurrentMonthPounds() {
     },
     {
       name: 'Direct Link',
-      value: deliveries.reduce(
-        (total, d) => (d.direct_donation_id ? total + d.report.weight : total),
-        0
-      ),
+      value: 0,
     },
   ]
 
@@ -148,25 +173,40 @@ export function CurrentMonthPounds() {
 
   const COLORS = ['#216810', '#9DA1A4', '#4EA528']
 
-  const onChange = e => {
+  const monthChange = e => {
     setCurrentMonth(parseInt(e.target.value))
   }
 
+  const monthOrYearChange = e => {
+    if (monthOrYear) setMonthOrYear(false)
+    else setMonthOrYear(true)
+  }
+
+  console.log(currentYear)
   return (
     <main id="Revamp">
       <section id="CurrentMonthPounds">
         <section id="Content">
-          <select style={{ marginTop: '3%' }}>
-            <option>Current Month Pounds</option>
-            <option>Current Year Pounds</option>
-          </select>
-          <select value={currentMonth} onChange={onChange} id="Month">
-            {months.map((month, i) => (
-              <option value={i} key={month}>
-                {months[i]}
-              </option>
-            ))}
-          </select>
+          {monthOrYear ? (
+            <>
+              <select style={{ marginTop: '3%' }} onChange={monthOrYearChange}>
+                <option>Current Month Pounds</option>
+                <option>Current Year Pounds</option>
+              </select>
+              <select value={currentMonth} onChange={monthChange} id="Month">
+                {months.map((month, i) => (
+                  <option value={i} key={month}>
+                    {months[i]}
+                  </option>
+                ))}
+              </select>{' '}
+            </>
+          ) : (
+            <select style={{ marginTop: '3%' }} onChange={monthOrYearChange}>
+              <option>Current Month Pounds</option>
+              <option selected="selected">Current Year Pounds</option>
+            </select>
+          )}
           <Text
             id="CurrentMonthPoundsLabel"
             type="secondary-header"
@@ -177,7 +217,7 @@ export function CurrentMonthPounds() {
           </Text>
           <section>
             <Text type="small" color="green">
-              {formatLargeNumber(emissionsReduced)}
+              {formatLargeNumber(totalMonthDeliveryPounds * 3.66)} lbs.
             </Text>
             <Text type="small" color="black">
               Emissions Reduced in Pounds
@@ -185,7 +225,7 @@ export function CurrentMonthPounds() {
           </section>
           <section>
             <Text type="small" color="green">
-              ${retailValue}
+              ${formatLargeNumber(retailValue)}
             </Text>
             <Text type="small" color="black">
               Retail Value
@@ -193,7 +233,7 @@ export function CurrentMonthPounds() {
           </section>
           <section>
             <Text type="small" color="green">
-              ${fairMarketValue}
+              ${formatLargeNumber(fairMarketValue)}
             </Text>
             <Text type="small" color="black">
               Fair Market Value
