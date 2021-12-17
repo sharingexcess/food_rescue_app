6ba84f5a0ef953440e73e9d401a5a3f15919caf1
Ryan McHenry <rmchenry@izotope.com>
2021-12-17 16:09:04 -0500

    using heckin semantic markdown

--- src/components/PickupReportInstructions/PickupReportInstructions.js
+++ src/components/PickupReportInstructions/PickupReportInstructions.js
@@ -1,26 +1,50 @@
-import { Text } from '@sharingexcess/designsystem'
+import { Button, Spacer, Text } from '@sharingexcess/designsystem'
+import { useApp } from 'hooks'
 
 export function PickupReportInstructions() {
+  const { setModal } = useApp()
   return (
     <div id="PickUpReportInstructions">
-      <Text type="subheader" color="black">
-        1. Acquire Sharing Excess scale from Donor Location. The scales have a
-        green SE sticker on the bottom. <br></br>
-        2. Place the scale on the ground only. Please do not put the scale in
-        the vehicle. <br></br>
-        3. Ensure the scale is zeroed (see button on scale). <br></br>
-        4. Place donations atop the scale. If you are weighing multiple boxes at
-        once, do NOT weigh more than two at one time. Ensure the contents are
-        within the same food category as listed on the weight entry form.{' '}
-        <br></br>
-        5. Record box weights in the weight entry form as described by category.
-        Each catrgory should host the total weight of all boxes within that
-        category. <br></br>
-        6. The pickup's total weight will automatically fill in under "Total
-        Weight"
-        <br></br>
-        7. Return scale to Donor location once weighing is complete.
+      <Text type="section-header">Reporting Instructions</Text>
+      <Spacer height={16} />
+      <Text type="small" color="black" align="left">
+        <ol>
+          <li>
+            Acquire Sharing Excess scale from Donor Location. The scales have a
+            green SE sticker on the bottom.
+          </li>
+          <li>
+            Place the scale on the ground only. Please do not put the scale in
+            the vehicle.
+          </li>
+          <li>Ensure the scale is zeroed (see button on scale).</li>
+          <li>
+            Place donations atop the scale. If you are weighing multiple boxes
+            at once, do NOT weigh more than two at one time. Ensure the contents
+            are within the same food category as listed on the weight entry
+            form.
+          </li>
+          <li>
+            Record box weights in the weight entry form as described by
+            category. Each category should host the total weight of all boxes
+            within that category.
+          </li>
+          <li>
+            The pickup's total weight will automatically fill in under "Total
+            Weight"
+          </li>
+          <li>Return scale to Donor location once weighing is complete.</li>
+        </ol>
       </Text>
+      <Spacer height={16} />
+      <Button
+        type="primary"
+        color="green"
+        fullWidth
+        handler={() => setModal(false)}
+      >
+        Cool, thanks!
+      </Button>
     </div>
   )
 }
