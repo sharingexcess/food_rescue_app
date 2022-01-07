57a577d3a88d1581982ed805e7e8c1782ee8b887
Armando Parra <ajp434@drexel.edu>
2022-01-07 14:48:26 -0500

    Function for Backup
    Rebase commit 09794ff

--- functions/index.js
+++ functions/index.js
@@ -39,4 +39,70 @@ exports.analytics = functions
     timeoutSeconds: 30,
     memory: '4GB',
   })
+<<<<<<< HEAD
   .https.onRequest(analytics_routes)
+=======
+  const { calendarId, eventId } = JSON.parse(request.body)
+
+  deleteEvent(calendarId, eventId, oAuth2Client)
+    .then(data => {
+      response.status(200).send(data)
+      return
+    })
+    .catch(err => {
+      console.error('Error adding event: ' + err.message)
+      response.status(500).send(ERROR_RESPONSE)
+      return
+    })
+}
+
+exports.backend = functions.https.onRequest(backend_routes)
+
+exports.backupToStorage = functions.pubsub
+  .schedule('02 00 * * 0')
+  .timeZone('America/New_York')
+  .onRun(() => {
+    // const { Storage } = require('@google-cloud/storage')
+    const storage = admin.storage().ref()
+    // const bucket = storage.bucket('')
+
+    const COLLECTIONS = [
+      'locations',
+      'organizations',
+      'rescues',
+      'stops',
+      'users',
+    ]
+
+    const today = new Date()
+    const date =
+      today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate()
+
+    COLLECTIONS.forEach(collection => {
+      // const options = {
+      //   destination: `${date}/${collection}.json`,
+      // }
+      const data = db.collection(collection).get()
+      const jsonData = JSON.stringify(data)
+
+      const blob = new Blob([jsonData], { type: 'application/json' })
+
+      const fileRef = storage.child(
+        `gs://sharing-excess-7e887.appspot.com/${date}/${collection}.json`
+      )
+
+      fileRef.put(blob)
+    })
+  })
+
+// exports.backupToStorage = functions.https.onRequest((request, response) => {
+//   const data = db.collection('locations').get()
+
+//   response.send(JSON.stringify(data))
+
+//   return true.catch(error => {
+//     console.error('Error adding document: ', error)
+//     return false
+//   })
+// })
+>>>>>>> 023a05c (Function for Backup)
