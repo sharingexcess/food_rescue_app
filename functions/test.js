c595f992c8277b7458d3759beaa8701443c1be7c
Ryan McHenry <rmchenry@izotope.com>
2022-02-01 17:47:36 -0500

    working test file

---
+++ functions/test.js
@@ -0,0 +1,44 @@
+// TODO: delete this file!
+const fetch = require('node-fetch')
+
+const locations = [
+  '880 N Bucknell St, Philadelphia PA 19130',
+  '514 S 12th St, Philadelphia PA',
+  '880 N Bucknell St, Philadelphia PA 19130',
+  '514 S 12th St, Philadelphia PA',
+  '514 S 12th St, Philadelphia PA',
+]
+
+const calculateTotalDistanceFromLocations = async (locations = []) => {
+  console.log(
+    'Running calculateTotalDistanceFromLocations with input:',
+    locations
+  )
+  const base_url =
+    'https://maps.googleapis.com/maps/api/distancematrix/json?units=imperial'
+  const API_KEY = ''
+
+  let total_distance = 0
+  let curr_location = locations.shift()
+
+  while (locations.length) {
+    const full_url = `
+      ${base_url}&key=${API_KEY}
+      &origins=${encodeURIComponent(curr_location)}
+      &destinations=${encodeURIComponent(locations[0])}
+    `
+
+    const response = await fetch(full_url).then(res => res.json())
+    const distance = parseInt(
+      response.rows[0].elements[0].distance.text.split(' ')[0]
+    )
+
+    console.log(distance)
+    total_distance += distance
+    curr_location = locations.shift()
+  }
+
+  console.log(total_distance)
+}
+
+calculateTotalDistanceFromLocations(locations)
