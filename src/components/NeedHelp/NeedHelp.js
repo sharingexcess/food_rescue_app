90388b5d4462b234675bf85e24343e394698991d
sn867 <sn867@drexel.edu>
2022-02-02 23:14:12 -0500

    Added dropdown and styling

--- src/components/NeedHelp/NeedHelp.js
+++ src/components/NeedHelp/NeedHelp.js
@@ -1,12 +1,16 @@
 import { useForm } from '@formspree/react'
-import { ExternalLink, Spacer, Text } from '@sharingexcess/designsystem'
-import { FORMSPREE_FORM_ID } from 'helpers'
+import {
+  ExternalLink,
+  Spacer,
+  Text,
+  Dropdown,
+} from '@sharingexcess/designsystem'
 import { useAuth } from 'hooks'
 import { useEffect, useState } from 'react'
 
 export function NeedHelp() {
   const { user } = useAuth()
-  const [state, handleSubmit] = useForm(FORMSPREE_FORM_ID)
+  const [state, handleSubmit] = useForm('myyoejgg')
   const [formData, setFormData] = useState({
     name: user.name,
     email: user.email,
@@ -41,7 +45,18 @@ export function NeedHelp() {
 
   return (
     <main id="NeedHelp">
-      <form action="https://formspree.io/f/myyoejgg" method="post">
+      <Spacer height={16} />
+      <form onSubmit={handleSubmit} method="POST">
+        <section id="Reasons">
+          <select>
+            <option>Reasons for needing help ...</option>
+            <option>Issue with the App</option>
+            <option>Can't Pickup All Food</option>
+            <option>Can't Contact Anyone at Location</option>
+            <option>Other</option>
+          </select>
+        </section>
+        <Spacer height={16} />
         <input type="hidden" value={user.name} name="name" />
         <input type="hidden" value={user.email} name="email" />
         <input type="hidden" value={user.phone} name="phone" />
@@ -65,7 +80,7 @@ export function NeedHelp() {
         <Spacer height={16} />
       </form>
       {state.succeeded && (
-        <Text type="small" align="center" color="white">
+        <Text type="small" align="center" color="green">
           Got it! Thank you so much for reaching out - the Sharing Excess team
           will get back to you ASAP.
         </Text>
