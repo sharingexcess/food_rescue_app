3b74262a8e88a17fbb9396eabf51960b7303da91
sn867 <sn867@drexel.edu>
2022-02-04 14:36:56 -0500

    Add need help button to delivery report

--- src/components/DeliveryReport/DeliveryReport.js
+++ src/components/DeliveryReport/DeliveryReport.js
@@ -6,11 +6,12 @@ import {
   STATUSES,
   updateImpactDataForRescue,
 } from 'helpers'
-import { Ellipsis, Input, Loading } from 'components'
-import { useAuth, useFirestore } from 'hooks'
+import { Ellipsis, Input, Loading, NeedHelp } from 'components'
+import { useAuth, useFirestore, useApp } from 'hooks'
 import { Button, Spacer, Text } from '@sharingexcess/designsystem'
 
 export function DeliveryReport() {
+  const { setModal, setModalState } = useApp()
   const { delivery_id, rescue_id } = useParams()
   const rescue = useFirestore('rescues', rescue_id)
   const navigate = useNavigate()
@@ -157,7 +158,13 @@ export function DeliveryReport() {
         </Button>
       ) : null}
       <Spacer height={16} />
-      <Button type="tertiary" color="white" size="large" fullWidth>
+      <Button
+        type="tertiary"
+        color="white"
+        size="large"
+        fullWidth
+        handler={() => setModal('NeedHelp')}
+      >
         Need Help ?
       </Button>
     </main>
