302ddabc52e622e39ab0b1ddf487ef4a6dcf9158
Oroghene Emudainohwo <oemudainohwo@gmail.com>
2022-08-10 14:20:21 -0400

    removed update operation from getActiveStop() in Rescue.utils.js

--- frontend/src/chakra_components/Rescue/Rescue.utils.js
+++ frontend/src/chakra_components/Rescue/Rescue.utils.js
@@ -1,24 +1,17 @@
-import { createTimestamp, SE_API, STATUSES } from 'helpers'
-import { useAuth } from 'hooks'
+import { STATUSES } from 'helpers'
 
-export async function getActiveStop(rescue) {
+export function getActiveStop(rescue) {
   if (!rescue) return null
-  const { user } = useAuth()
   let activeStop = null
   for (const stop of rescue.stops) {
-    if (stop.status !== 'cancelled' && stop.status !== 'completed') {
-      await SE_API.post(
-        `/stops/${stop.id}/update`,
-        {
-          status: STATUSES.ACTIVE,
-          timestamp_logged_start: createTimestamp(),
-          timestamp_updated: createTimestamp(),
-        },
-        user.accessToken
-      )
+    if (
+      stop.status !== STATUSES.CANCELLED &&
+      stop.status !== STATUSES.COMPLETED
+    ) {
       activeStop = stop
       break
     }
   }
+
   return activeStop
 }
