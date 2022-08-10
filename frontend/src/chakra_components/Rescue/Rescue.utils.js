5e6da5bfcf91ef46235552d1acab8229895b7936
Oroghene Emudainohwo <oemudainohwo@gmail.com>
2022-08-10 12:55:43 -0400

    stops updating status to active on backend

--- frontend/src/chakra_components/Rescue/Rescue.utils.js
+++ frontend/src/chakra_components/Rescue/Rescue.utils.js
@@ -1,8 +1,21 @@
-export const getActiveStop = rescue => {
+import { createTimestamp, SE_API, STATUSES } from 'helpers'
+import { useAuth } from 'hooks'
+
+export async function getActiveStop(rescue) {
   if (!rescue) return null
+  const { user } = useAuth()
   let activeStop = null
   for (const stop of rescue.stops) {
     if (stop.status !== 'cancelled' && stop.status !== 'completed') {
+      await SE_API.post(
+        `/stops/${stop.id}/update`,
+        {
+          status: STATUSES.ACTIVE,
+          timestamp_logged_start: createTimestamp(),
+          timestamp_updated: createTimestamp(),
+        },
+        user.accessToken
+      )
       activeStop = stop
       break
     }
