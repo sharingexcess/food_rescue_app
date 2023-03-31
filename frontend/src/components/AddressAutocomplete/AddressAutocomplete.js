d33ede67837242011b1a9c4bac41ee2d6c88eb9c
Ryan McHenry <ryanmarshallmc@gmail.com>
2023-03-31 17:36:55 -0400

    fixing google maps autocomplete

--- frontend/src/components/AddressAutocomplete/AddressAutocomplete.js
+++ frontend/src/components/AddressAutocomplete/AddressAutocomplete.js
@@ -1,47 +1,54 @@
 import { useEffect, useState } from 'react'
 import { Input } from '@chakra-ui/react'
+import { Helmet } from 'react-helmet'
 
 export function AddressAutocomplete({ handleSelect }) {
   const [query, setQuery] = useState('')
 
   useEffect(() => {
-    const autoComplete = new window.google.maps.places.Autocomplete(
-      document.getElementById('autocomplete'),
-      {
-        componentRestrictions: { country: 'us' },
-      }
-    )
-    autoComplete.addListener('place_changed', () => {
-      try {
-        const addressObject = autoComplete.getPlace()
-        const query = addressObject.formatted_address
-        setQuery(query)
-        const parsedAddressObject = {
-          address1:
-            addressObject.address_components.find(a =>
-              a.types.includes('street_number')
-            ).long_name +
-            ' ' +
-            addressObject.address_components.find(a =>
-              a.types.includes('route')
-            ).long_name,
-          city: getCityFromAddressObject(addressObject),
-          state: addressObject.address_components.find(a =>
-            a.types.includes('administrative_area_level_1')
-          ).short_name,
-          zip: addressObject.address_components.find(a =>
-            a.types.includes('postal_code')
-          ).long_name,
-          lat: addressObject.geometry.location.lat(),
-          lng: addressObject.geometry.location.lng(),
+    if (!window.google?.maps) {
+      setTimeout(configureAutocomplete, 500)
+    } else configureAutocomplete()
+
+    function configureAutocomplete() {
+      const autoComplete = new window.google.maps.places.Autocomplete(
+        document.getElementById('autocomplete'),
+        {
+          componentRestrictions: { country: 'us' },
         }
-        handleSelect(parsedAddressObject)
-      } catch (e) {
-        console.error(e)
-        alert('Could not use this address, please select another!')
-        setQuery('')
-      }
-    })
+      )
+      autoComplete.addListener('place_changed', () => {
+        try {
+          const addressObject = autoComplete.getPlace()
+          const query = addressObject.formatted_address
+          setQuery(query)
+          const parsedAddressObject = {
+            address1:
+              addressObject.address_components.find(a =>
+                a.types.includes('street_number')
+              ).long_name +
+              ' ' +
+              addressObject.address_components.find(a =>
+                a.types.includes('route')
+              ).long_name,
+            city: getCityFromAddressObject(addressObject),
+            state: addressObject.address_components.find(a =>
+              a.types.includes('administrative_area_level_1')
+            ).short_name,
+            zip: addressObject.address_components.find(a =>
+              a.types.includes('postal_code')
+            ).long_name,
+            lat: addressObject.geometry.location.lat(),
+            lng: addressObject.geometry.location.lng(),
+          }
+          handleSelect(parsedAddressObject)
+        } catch (e) {
+          console.error(e)
+          alert('Could not use this address, please select another!')
+          setQuery('')
+        }
+      })
+    }
   }, []) //eslint-disable-line
 
   function getCityFromAddressObject(addressObject) {
@@ -57,12 +64,19 @@ export function AddressAutocomplete({ handleSelect }) {
   }
 
   return (
-    <Input
-      id="autocomplete"
-      type="text"
-      onChange={e => setQuery(e.target.value)}
-      value={query}
-      placeholder="Start typing an address..."
-    />
+    <>
+      <Helmet>
+        <script
+          src={`https://maps.googleapis.com/maps/api/js?key=${process.env.REACT_APP_FIREBASE_API_KEY}&libraries=places`}
+        ></script>
+      </Helmet>
+      <Input
+        id="autocomplete"
+        type="text"
+        onChange={e => setQuery(e.target.value)}
+        value={query}
+        placeholder="Start typing an address..."
+      />
+    </>
   )
 }
